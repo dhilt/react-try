55f0148bbe24cd766cd4fcf472012a986e175b13
Den <yuvzhenko@inbox.ru>
2017-05-12 20:39:32 +0800

    Fixed Database setup

--- server/db.js
+++ server/db.js
@@ -2,7 +2,13 @@ var fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
 var passwordHash = require('password-hash');
 
-var db = new sqlite3.Database(':memory:');
+if (fs.existsSync('server/development.db')) {
+  fs.unlink('server/development.db', function () {
+    console.log('Database was deleted!');
+  });
+}
+
+var db = new sqlite3.Database('server/development.db');
 db.serialize(function () {
   db.run('CREATE TABLE User (id INTEGER PRIMARY KEY, login TEXT NOT NULL, hash TEXT NOT NULL)');
   var adminHash = passwordHash.generate('password34');
@@ -10,11 +16,3 @@ db.serialize(function () {
   stmt.finalize();
 });
 db.close();
-
-if (fs.existsSync('server/development.db')) {
-  fs.unlink('server/development.db', function () {
-    console.log('Database was deleted!');
-  });
-}
-
-fs.writeFile('server/development.db', db, function () {});
