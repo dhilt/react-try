c6a6a031fb90751f1ab4ef750083480a6ca58838
Den <yuvzhenko@inbox.ru>
2017-05-14 01:37:31 +0800

    Database login api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var sqlite3 = require('sqlite3');
+var passwordHash = require('password-hash');
 var port = 3003;
 
 var db = new sqlite3.Database('server/development.db', sqlite3.OPEN_READONLY, function (Error) {
@@ -23,6 +24,34 @@ app.get('/api/test', function (req, res) {
       res.send({success: false, error: err});
     };
   });
+});
+
+app.post('/api/login', function (req, res) {
+  var login = req.query.login;
+  var password = req.query.password;
+  if (login === undefined || password === undefined) {
+    res.send({error: 'No login or password!'});
+    return;
+  };
+
+  db.all('SELECT id, login, hash FROM User', function(err, rows) {
+    if (err) {
+        res.send({success: false, error: err});
+    } else {
+      var row = rows.find(function(element) {
+        return element.login = login;
+      });
+
+      var access = passwordHash.verify(password, row.hash);
+      if (access)
+        res.send({id: row.id, login: row.login});
+      else
+        res.send({success: false, error: 'Invalid password.'})
+    };
+  });
+//  db.get('SELECT id, login = ?, hash FROM User', login, function(err, row) {
+//    res.send({id: row.id, login: row.login, hash: row.hash});
+//  });
 });
 
 app.listen(port, function () {
