ea9fd6c974bc0815648a182881f0eebf88f20d37
Den <yuvzhenko@inbox.ru>
2017-05-13 14:58:47 +0800

    Make global 'db' variable. Added error handling.

--- server/index.js
+++ server/index.js
@@ -3,22 +3,26 @@ var app = express();
 var sqlite3 = require('sqlite3');
 var port = 3003;
 
+var db = new sqlite3.Database('server/development.db', sqlite3.OPEN_READONLY, function (Error) {
+  if (Error !== null)
+    throw ('No development.db found. Run "node db.js"...');
+});
+
 app.get('/api/test', function (req, res) {
   var users = [];
   var user = {};
-  var db = new sqlite3.Database('server/development.db', sqlite3.OPEN_READONLY, function (Error) {
-    if (Error !== null)
-      throw ('No development.db found. Run "node db.js"...');
-  });
 
   db.all('SELECT * FROM User', function (err, rows) {
-    rows.forEach(function (row) {
-      user = {id: row.id, login: row.login, hash: row.hash};
-      users.push(user);
-    });
-    res.send(users);
+    if (!err) {
+      rows.forEach(function (row) {
+        user = {id: row.id, login: row.login, hash: row.hash};
+        users.push(user);
+      });
+      res.send({success: true, result: users});
+    } else {
+      res.send({success: false, error: err});
+    };
   });
-  db.close();
 });
 
 app.listen(port, function () {
