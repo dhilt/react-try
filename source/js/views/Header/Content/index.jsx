550335d410f0e2683ac30dcc78dee7433d045834
Nastya Stavitskaya <nastycorps@gmail.com>
2017-05-15 21:19:46 -0700

    Add img and style to header

---
+++ source/js/views/Header/Content/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Info from './Info';
+
+export default class Content extends Component {
+
+  render() {
+    return (
+      <div className='Content'>
+        <Info />
+      </div>
+    );
+  }
+}
