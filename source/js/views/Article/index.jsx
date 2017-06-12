f78d4aaae6b39e979544e9b048bf0ae746451987
butcheruga <yuvzhenko@inbox.ru>
2017-06-13 02:34:25 +0800

    Defending render. And style for Article.

--- source/js/views/Article/index.jsx
+++ source/js/views/Article/index.jsx
@@ -19,28 +19,46 @@ export default class Article extends Component {
 
   constructor() {
     super();
+    this.makeArticle = this.makeArticle.bind(this);
   }
 
   componentWillMount() {
     this.props.dispatch(getArticleAsync(this.props.params.id));
   }
 
-  render() {
-    let { data } = this.props;
+  makeArticle(article) {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const time = new Date(data.get('createdAt'));
+    const time = new Date(article.get('createdAt'));
+    const articleYear = time.getFullYear();
     const articleMonth = time.getMonth();
     const articleDay = time.getDate();
-    const Article = <div>
-                      <p>Date: {articleDay} {months[articleMonth]}</p>
-                      <p>Author: {data.get('userName')}</p>
-                      <p>{data.get('title')}</p>
-                      <p>{data.get('description')}</p>
-                      <p>{data.get('text')}</p>
-                    </div>;
+    return <div className='wrapArticle'>
+             <p className='titleArticle'>{article.get('title')}</p>
+             <p className='dateArticle'>Date: {articleDay} {months[articleMonth]}, {articleYear}</p>
+             <p className='authorArticle'>Author: {article.get('userName')}</p>
+             <p className='descriptionArticle'>{article.get('description')}</p>
+             <img className='imageArticle' src={article.get('image')} />
+             <p className='textArticle'>{article.get('text')}</p>
+           </div>
+  }
 
-    return (
-      <div>{Article}</div>
+  render() {
+    let { data, pending, error } = this.props;
+
+    return !data ? (
+      pending ? (
+        <div className='wrapArticle'>
+          <div className='ArticlePreloader'></div>
+        </div>
+        ) : (
+        <div className='wrapArticle'>
+          <p>{error}</p>
+        </div>
+        )
+      ) : (
+      <div>
+        {this.makeArticle(data)}
+      </div>
     );
   }
 }
