b7aad519fe74d208f3071257f4113b357ac12e67
dhilt <d.alexanov@gmail.com>
2017-07-01 07:04:22 +0300

    new article refactoring

--- source/js/views/_admin/NewArticle/index.jsx
+++ source/js/views/_admin/NewArticle/index.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { actions } from 'react-redux-form/immutable'
 
-import { createArticleAsync } from 'actions/_admin/newArticle';
-import { ArticleForm } from '../ArticleForm';
+import { createArticleAsync } from 'actions/_admin/newArticle'
+import { ArticleForm } from '../ArticleForm'
 
 @connect(state => ({
   newArticle: state._adminNewArticle,
@@ -12,32 +13,40 @@ import { ArticleForm } from '../ArticleForm';
 export default class NewArticle extends Component {
 
   constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
+    super()
+    this.onDateChanged = this.onDateChanged.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  onDateChanged(value, model, validators) {
+    const { dispatch } = this.props
+    dispatch(actions.change(model, value))
+    dispatch(actions.validate(model, validators(value)))
   }
 
   handleSubmit() {
-    const { dispatch } = this.props;
-    dispatch(createArticleAsync());
+    const { dispatch } = this.props
+    dispatch(createArticleAsync())
   }
 
   render() {
-    let { newArticle, newArticleModel, newArticleForm } = this.props;
-    let pending = newArticle.get('pending');
-    let pristine = newArticleForm.$form && newArticleForm.$form.pristine;
-    let valid = newArticleForm.$form && newArticleForm.$form.valid;
+    let { newArticle, newArticleModel, newArticleForm } = this.props
+    let pending = newArticle.get('pending')
+    let pristine = newArticleForm.$form && newArticleForm.$form.pristine
+    let valid = newArticleForm.$form && newArticleForm.$form.valid
 
     return (
       <div>
         <h2>Create new Article...(admin only)</h2>
         <ArticleForm
-          data={''}
+          data={newArticleModel.toJS()}
+          onDateChanged={this.onDateChanged}
           onSubmit={this.handleSubmit}
           model={'newArticleModel'}
           pending={pending}
           pristine={pristine}
           valid={valid} />
       </div>
-    );
+    )
   }
 }
