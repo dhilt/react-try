b5506924e158d93ec32de5c29baf8268c85493d5
BitDen <yuvzhenko@inbox.ru>
2017-12-28 08:34:16 +0800

    Some changes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,50 +1,50 @@
 const db = require('../helpers/db.helper.js')
 const AuthHelper = require('../helpers/auth.helper.js')
 
-let AuthController = {
-
-  getUserInfo: (req) => new Promise((resolve, reject) =>
+module.exports.getUserInfo = (req) =>
+  new Promise((resolve, reject) =>
     AuthHelper.doAuthorize(req.headers.authorization).then(
       user => resolve(user),
       error => reject(error)
     )
-  ),
+  )
 
-  getAuthorization: (req) =>
-    new Promise((resolve, reject) => {
-      const login = req.body.login,
-            password = req.body.password
-      if (!login) {
-        throw `Bad parameters. No login.`
-      }
-      if (!password) {
-        throw `Bad parameters. No password.`
-      }
-      return resolve({ login, password })
-    })
-    // take user info
-    .then(params =>
-      db.get('SELECT * FROM User WHERE login = ?', params.login).then(
-        user => {
-          if (!user) {
-            throw `That user doesn't exist.`
-          }
-          if (!AuthHelper.verifyPassword(params.password, user.hash)) {
-            throw `Invalid password.`
-          }
-          return Promise.resolve(user)
-        },
-        error => { throw `Database error. Can't authorize user.` }
-      )
+module.exports.getAuthorization = (req) =>
+  new Promise((resolve, reject) => {
+    const login = req.body.login,
+          password = req.body.password
+    if (!login) {
+      throw `Bad parameters. No login.`
+    }
+    if (!password) {
+      throw `Bad parameters. No password.`
+    }
+    return resolve({ login, password })
+  })
+  // take user info
+  .then(params =>
+    db.get('SELECT * FROM User WHERE login = ?', params.login).then(
+      user => {
+        if (!user) {
+          throw `That user doesn't exist.`
+        }
+        if (!AuthHelper.verifyPassword(params.password, user.hash)) {
+          throw `Invalid password.`
+        }
+        return Promise.resolve(user)
+      },
+      error => { throw `Database error. Can't authorize user.` }
     )
-    // send result
-    .then(user => {
-      const userInfo = AuthHelper.getUserInfo(user)
-      const token = AuthHelper.signToken(userInfo)
-      return Promise.resolve({ userInfo, token })
-    }),
+  )
+  // send result
+  .then(user => {
+    const userInfo = AuthHelper.getUserInfo(user)
+    const token = AuthHelper.signToken(userInfo)
+    return Promise.resolve({ userInfo, token })
+  })
 
-  testUsers: (req) => new Promise((resolve, reject) =>
+module.exports.testUsers = (req) =>
+  new Promise((resolve, reject) =>
     db.all('SELECT * FROM User').then(
       rows => {
         let users = []
@@ -54,6 +54,3 @@ let AuthController = {
       error => reject(error)
     )
   )
-}
-
-module.exports = AuthController
--- server/controllers/games.controller.js
+++ server/controllers/games.controller.js
@@ -5,30 +5,25 @@ const GAMES = {
   dashboardCount: 5
 }
 
-let GamesController = {
-
-  getGames: (req) =>
-    new Promise((resolve, reject) => {
-      const dashboard = req.query.hasOwnProperty('dashboard')
-      let count = Number(req.query.count) || GAMES.defaultCount
-      if (isNaN(count) || count <= 0) {
-        throw `Can't get games. Bad entity param (count)`
-      }
-      let orderBy = 'createdAt'
-      let orderDir = 'DESC'
-      if (dashboard) {
-        count = GAMES.dashboardCount
-      }
-      const ordering = (orderBy ? ' ORDER BY ' + orderBy + ' ' : '') + (orderBy && orderDir ? orderDir : '')
-      return resolve({ count, ordering })
-    })
-    // get games
-    .then(params =>
-      db.all('SELECT * FROM Game' + params.ordering + ' LIMIT ?', params.count).then(
-        games => Promise.resolve(games),
-        error => { throw `Can't get games. Database error (select)`}
-      )
+module.exports.getGames = (req) =>
+  new Promise((resolve, reject) => {
+    const dashboard = req.query.hasOwnProperty('dashboard')
+    let count = Number(req.query.count) || GAMES.defaultCount
+    if (isNaN(count) || count <= 0) {
+      throw `Can't get games. Bad entity param (count)`
+    }
+    let orderBy = 'createdAt'
+    let orderDir = 'DESC'
+    if (dashboard) {
+      count = GAMES.dashboardCount
+    }
+    const ordering = (orderBy ? ' ORDER BY ' + orderBy + ' ' : '') + (orderBy && orderDir ? orderDir : '')
+    return resolve({ count, ordering })
+  })
+  // get games
+  .then(params =>
+    db.all('SELECT * FROM Game' + params.ordering + ' LIMIT ?', params.count).then(
+      games => Promise.resolve(games),
+      error => { throw `Can't get games. Database error (select)`}
     )
-}
-
-module.exports = GamesController
+  )
