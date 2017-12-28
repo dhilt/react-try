3f0997c437968e2f2e4c9e78fef67e1da668612f
BitDen <yuvzhenko@inbox.ru>
2017-12-28 13:48:58 +0800

    Downsize upto ~500 bytes c:

--- source/js/views/Footer/index.jsx
+++ source/js/views/Footer/index.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 
-import SocialTable from './socialTable'
-import BottomImages from './bottomImages'
-
 export default Footer =>
   <div className="page-footer">
-    <SocialTable />
-    <BottomImages />
+
+    <div className="social-table">
+      <hr className="yellow-top-line" />
+      <div className="running-man-picture" />
+      <ul className="social-links">
+        <a href="http://www.twitter.com" />
+        <a href="http://www.facebook.com" />
+        <a href="http://www.vk.com" />
+        <a href="http://www.google.com" />
+        <a href="http://www.twitter.com" />
+      </ul>
+      <div className="footer-about-site">
+        <p>{'Немного о нашем сайте'}</p>
+        <pre>
+          {'AM4 - это большая коллекция игровой информации, помогающей геймерам со всего мира быть в курсе последних новостей игрового мира.'}
+        </pre>
+      </div>
+      <button
+        onClick={() => alert('This functionality is missing.')}
+        className="button-write-us">
+        {'Написать нам'}
+      </button>
+    </div>
+
+    <div className="bottom-images">
+      <div className="table-age-limit">{'18+'}</div>
+      <div className="table-copyright">
+        <p>{'Copyright © 2001-2014'}</p>
+        <pre>
+          {'Все права защищены законодательством РФ. Использование материалов сайта возможно только с прямой ссылкой на источник.'}
+        </pre>
+      </div>
+      <div className="footer-guy-with-guns" />
+      <div className="footer-rectangles" />
+      <hr className="bottom-red-line" />
+      <hr className="bottom-blue-line" />
+    </div>
+
   </div>
