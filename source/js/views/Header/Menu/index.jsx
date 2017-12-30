d8dae3dfc85cb53692ec3bda16c7ae8eef57db89
BitDen <yuvzhenko@inbox.ru>
2017-12-30 14:43:32 +0800

    lol

--- source/js/views/Header/Menu/index.jsx
+++ source/js/views/Header/Menu/index.jsx
@@ -10,11 +10,11 @@ export default class Menu extends Component {
         <Link to='/'>
           <img src='/assets/img/logo.png' />
         </Link>
-        <Link to='/about'>{'О проекте'}</Link>
+        <Link to='/news'>{'Новости'}</Link>
         <Link to='/articles'>{'Статьи'}</Link>
         <Link to='/videos'>{'Видео'}</Link>
-        <Link to='/news'>{'Новости'}</Link>
         <Link to='/games'>{'Игры'}</Link>
+        <Link to='/about'>{'О проекте'}</Link>
         <AuthContainer />
       </div>
     )
